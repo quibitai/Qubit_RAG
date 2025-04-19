5c56689a3b1a74b3aa3aa2f70771a7d0400d7312
quibitai <your.email@example.com>
2025-04-19 11:47:20 -0500

    Fix unescaped quotes in greeting component

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -47,7 +47,7 @@ export const Greeting = () => {
         transition={{ delay: 0.6 }}
         className="text-2xl text-zinc-500"
       >
-        "{randomQuote}"
+        &ldquo;{randomQuote}&rdquo;
       </motion.div>
       <motion.div
         initial={{ opacity: 0, y: 10 }}
